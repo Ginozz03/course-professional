3abacd362bc24463f72f09f7b8b67524d5dfbb20
gianmarco <gianmarco.zin@gmail.com>
2021-03-20 20:55:35 +0100

    completed frontend + maven plugin

--- src/main/java/com/projectcourse/courseprofessional/student/Gender.java
+++ src/main/java/com/projectcourse/courseprofessional/student/Gender.java
@@ -3,15 +3,5 @@ package com.projectcourse.courseprofessional.student;
 public enum Gender {
     MALE,
     FEMALE,
-    LESBIAN,
-    GAY,
-    BISEXUAL,
-    TRANSEXUAL,
-    QUESTIONING,
-    QUEER,
-    INTERSEX,
-    PANSEX,
-    TWOSPIRIT,
-    ANDROGYNOUS,
-    ASEXUAL
+    OTHER
 }
